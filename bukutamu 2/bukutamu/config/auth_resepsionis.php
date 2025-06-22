c39936d2f723ce4cab9db497e54afb85de852f1d
nichodakkar <152762837+nichodakkar@users.noreply.github.com>
2025-06-23 00:49:31 +0700

    Delete bukutamu 2/bukutamu directory

--- bukutamu 2/bukutamu/config/auth_resepsionis.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'resepsionis') {
-  header("Location: ../login.php");
-  exit();
-}
--- bukutamu 2/bukutamu/hash.php
+++
@@ -1,6 +0,0 @@
-<?php
-$password = 'admin123'; // ganti dengan password asli yang kamu inginkan
-$hashed = password_hash($password, PASSWORD_DEFAULT);
-echo "Password asli: $password<br>";
-echo "Hash-nya: $hashed";
-?>
